30f453569a8a4f6a9be43b7a6661d9ceed5930d2
DanielAristy <danielaristy22@gmail.com>
2021-06-09 23:23:53 -0500

    Primera fase de la refactorizacion utilizando extract metodos y utlizando métodos alternos para hacer validaciones simples, resta, mayor que, menor que, mayor o igual, menor o igual

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -8,6 +8,7 @@ public class TennisGame2 implements TennisGame
     public String P2res = "";
     private String player1Name;
     private String player2Name;
+    private String palabras[] = {"Fifteen","Thirty","Forty"};
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -16,88 +17,98 @@ public class TennisGame2 implements TennisGame
 
     public String getScore(){
         String score = "";
-        if (P1point == P2point && P1point < 4)
+        if (isSameNumber(P1point,P2point) && isLessThan(P1point,4))
         {
-            if (P1point==0)
+            if (isSameNumber(P1point,0))
                 score = "Love";
-            if (P1point==1)
+            if (isSameNumber(P1point,1))
                 score = "Fifteen";
-            if (P1point==2)
+            if (isSameNumber(P1point,2))
                 score = "Thirty";
             score += "-All";
         }
-        if (P1point==P2point && P1point>=3)
+        if (isSameNumber(P1point,P2point)&& isGreaterOrEqualThan(P1point,3))
             score = "Deuce";
         
-        if (P1point > 0 && P2point==0)
+        if (isGreaterThan(P1point,0) && isSameNumber(P2point,0))
         {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
+            //mayor> 0 o menor que cuatro
+            P1res = (isGreaterOrEqualThan(P1point,0) && isLessThan(P1point,4))? palabras[P1point - 1] : "";
             P2res = "Love";
             score = P1res + "-" + P2res;
         }
-        if (P2point > 0 && P1point==0)
+        if (isGreaterThan(P2point,0) && isSameNumber(P1point,0))
         {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
+            P2res = (isGreaterOrEqualThan(P2point,0) && isLessThan(P2point,4))? palabras[P2point - 1] : "";
             P1res = "Love";
             score = P1res + "-" + P2res;
         }
-        
-        if (P1point>P2point && P1point < 4)
+
+
+        if (isGreaterThan(P1point,P2point) && isLessThan(P1point,4))
         {
-            if (P1point==2)
+
+            if (isSameNumber(P1point,2))
                 P1res="Thirty";
-            if (P1point==3)
+            if (isSameNumber(P1point,3))
                 P1res="Forty";
-            if (P2point==1)
+            if (isSameNumber(P2point,1))
                 P2res="Fifteen";
-            if (P2point==2)
+            if (isSameNumber(P2point,2))
                 P2res="Thirty";
             score = P1res + "-" + P2res;
         }
-        if (P2point>P1point && P2point < 4)
+        if (isGreaterThan(P2point,P1point) && isLessThan(P2point,4))
         {
-            if (P2point==2)
+            if (isSameNumber(P2point,2))
                 P2res="Thirty";
-            if (P2point==3)
+            if (isSameNumber(P2point,3))
                 P2res="Forty";
-            if (P1point==1)
+            if (isSameNumber(P1point,1))
                 P1res="Fifteen";
-            if (P1point==2)
+            if (isSameNumber(P1point,2))
                 P1res="Thirty";
             score = P1res + "-" + P2res;
         }
-        
-        if (P1point > P2point && P2point >= 3)
+        // es mayor que && es mayor igual que
+        if (isGreaterThan(P1point,P2point) && isGreaterOrEqualThan(P2point,3))
         {
             score = "Advantage player1";
         }
-        
-        if (P2point > P1point && P1point >= 3)
+        // es mayor que && es mayor igual que
+        if (isGreaterThan(P2point,P1point)&& isGreaterOrEqualThan(P1point,3))
         {
             score = "Advantage player2";
         }
-        
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
+
+        if (isGreaterOrEqualThan(P1point,4) && isGreaterOrEqualThan(P2point,0) && isGreaterOrEqualThan(substractNumbers(P1point,P2point),2))
         {
             score = "Win for player1";
         }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
+        if (isGreaterOrEqualThan(P2point, 4) &&  isGreaterOrEqualThan(P1point,0) && isGreaterOrEqualThan(substractNumbers(P2point,P1point),2))
         {
             score = "Win for player2";
         }
         return score;
+    }
+
+    private boolean isLessThan(int scorePlayer1, int minorNumber) {
+        return scorePlayer1 < minorNumber;
+    }
+
+    private int substractNumbers(int number1, int number2){
+        return number1 - number2;
+    }
+    private boolean isSameNumber(int scorePlayer1, int scorePlayer2) {
+        return scorePlayer1 == scorePlayer2;
+    }
+
+    private boolean isGreaterOrEqualThan(int scorePlayer1, int biggerNumber) {
+        return scorePlayer1 >= biggerNumber;
+    }
+
+    private boolean isGreaterThan(int scorePlayer1, int biggerNumber) {
+        return scorePlayer1 > biggerNumber;
     }
     
     public void P1Score(){
