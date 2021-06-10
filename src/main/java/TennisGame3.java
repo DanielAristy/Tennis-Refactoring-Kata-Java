1b57f127916660683561baed6dead5a0267a38a4
DanielAristy <danielaristy22@gmail.com>
2021-06-10 17:41:49 -0500

    Cambio del nombre de las variables por unas mas intuitivas y se utiliza la extraccion de métodos

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,35 +1,63 @@
 
 public class TennisGame3 implements TennisGame {
     
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int playerTwoPoints;
+    private int playerOnePoints;
+    private String namePlayerOne;
+    private String namePlayerTwo;
+    private String[] palabras = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame3(String namePlayerOne, String namePlayerTwo) {
+        this.namePlayerOne = namePlayerOne;
+        this.namePlayerTwo = namePlayerTwo;
     }
-
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        String response;
+
+        if (isLessThat(playerOnePoints) && isLessThat(playerTwoPoints)
+                && !(toAddNumbers(playerOnePoints, playerTwoPoints) == 6)) {
+
+            response = palabras[playerOnePoints];
+            return isSameThan(playerOnePoints, playerTwoPoints) ? response + "-All" : response + "-" + palabras[playerTwoPoints];
         } else {
-            if (p1 == p2)
+
+            if (isSameThan(playerOnePoints, playerTwoPoints))
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            response = isGreaterThat(playerOnePoints, playerTwoPoints) ? namePlayerOne : namePlayerTwo;
+            return (squareNumber(subtractNumbers(playerOnePoints, playerTwoPoints)) == 1) ? "Advantage " + response : "Win for " + response;
         }
     }
+
+    private int squareNumber(int number){
+        return (int) Math.pow(number,2);
+    }
+
+    private int subtractNumbers(int numberOne, int numberTwo) {
+        return numberOne - numberTwo;
+    }
+
+    private boolean isGreaterThat(int numberOne, int numberTwo) {
+        return numberOne > numberTwo;
+    }
+
+    private boolean isSameThan(int numberOne, int numberTwo) {
+        return numberOne == numberTwo;
+    }
+
+    private int toAddNumbers(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    private boolean isLessThat(int number) {
+        return number < 4;
+    }
     
+
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.playerOnePoints += 1;
         else
-            this.p2 += 1;
+            this.playerTwoPoints += 1;
         
     }
 
